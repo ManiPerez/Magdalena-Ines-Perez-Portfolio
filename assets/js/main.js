d548e7c25ffbb1784aa3d4bbc4ade6d99952c14e
Magdalena <perezmagdalenaines@gmail.com>
2021-12-12 16:20:47 -0300

    creating my portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,27 +31,201 @@ function linkAction() {
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header');
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className;
+
+    for (i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close';
+    }
+    if (itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open';
+    }
+} 
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click',toggleSkills);
+})
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]');
 
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active');
+        })
+        target.classList.add('qualification__active');
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active');
+        })
+        tab.classList.add('qualification__active');
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close');
+ 
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal');
+}
 
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', ()=> {
+        modal(i);
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', ()=> {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal');
+        })
+    })
+})
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiperPortfolio = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }
+});
 
+/*==================== TOOLS ====================*/
+let swiperTools = new Swiper(".tools__container", {
+    loop: true,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+        slidesPerView: 2,
+        },
+        350: {
+        slidesPerView: 3,
+        },
+        450: {
+        slidesPerView: 4,
+        },
+        550: {
+        slidesPerView: 5,
+        },
+        650: {
+        slidesPerView: 6,
+        },
+        780: {
+        slidesPerView: 7,
+        }  
+    }
+});
 
-/*==================== TESTIMONIAL ====================*/
+/*==================== CONTACT ME form ====================*/
+const form = document.querySelector('form'),
+statusTxt = form.querySelector('.contact__btn-msg');
 
+form.onsubmit = (e) => {
+    e.preventDefault(); // prevent form from submitting
+    statusTxt.style.display = 'block';
+
+    let xhr =new XMLHttpRequest(); // creating new xml object
+    xhr.open("POST", "message.php", true); // sending POST request to message.php file
+    xhr.onload = () =>{ // once ajax loaded
+        if (xhr.readyState == 4 && xhr.status == 200){ // if ajax  response status is 200 & ready status is 4 means there is no error
+            let response = xhr.response; // storing ajax response in a response variable
+            console.log(response);
+        }
+    }
+    let formData = new FormData(form); //creating new formData obj. This obj is used to send form data
+    xhr.send(formData); //sending form data
+}
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]');
 
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
-
+function scrollHeader(){
+    const nav = document.getElementById('header');
+    // when the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/ 
-
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // when the scroll is higher than 560 viewport height, add the show-scroll class to a tag with the scroll
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
 
 /*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+// We validate if the user previously chose a topic
+if (selectedTheme){
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or desactivated the dark theme
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+// Activate / desactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    // We save the same and the current icon that the user chose
+    localStorage.getItem('selected-theme', getCurrentTheme());
+    localStorage.getItem('selected-icon', getCurrentIcon());
+})
+
+/*================== SCROLL REVEAL ANIMATION ==================*/
+// const sr = scrollReveal({
+//     origin: 'top',
+//     distance: '30px',
+//     duration: 2000,
+//     reset: true
+// });
---
+++ message.php
@@ -0,0 +1,9 @@
+<?php
+    // let's get all form values
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $project = $_POST['project'];
+    $message = $_POST['message'];
+
+    echo $name;
+?>
